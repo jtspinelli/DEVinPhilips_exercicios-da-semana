8acd8c56d2542a961c5acfb58b9dd385f0ca42af
jtspinelli <contato@jonathanspinelli.com>
2022-10-21 20:45:36 -0300

    refactor M1S03 ex3.js and ex4.js

--- M1S03/scripts/ex3.js
+++ M1S03/scripts/ex3.js
@@ -1,9 +1,7 @@
 function exercicio3(){
     alert("Este exercício aparece no console.")
     
-    let concatenarArrays = (array1, array2) => {
-        return [...array1, ...array2];
-    }
+    let concatenarArrays = (array1, array2) => [...array1, ...array2];
 
     let someNumbers = [1, 2, 3];
     let moreNumbers = [4, 5, 6];
