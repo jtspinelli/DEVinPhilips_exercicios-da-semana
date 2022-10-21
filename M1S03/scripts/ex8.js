9afb761c49e0db4ebe87cc69a075eca9bf9b3132
jtspinelli <contato@jonathanspinelli.com>
2022-10-21 01:01:29 -0300

    add M1S03 ex8.js

---
+++ M1S03/scripts/ex8.js
@@ -0,0 +1,10 @@
+function exercicio8() {
+    alert("Este exercício aparece no console.")
+    
+    const uneDobraRetorna = (numArray,...numeros) => {
+        return numArray.concat(numeros.map(num => { return num * 2 }));
+    }
+
+    console.log(`uneDobraRetorna([1,2,3], 4, 4): \n${uneDobraRetorna([1,2,3], 4, 4)}\n\n`);
+    console.log(`uneDobraRetorna([5,6], 10, 12, 25): \n${uneDobraRetorna([5,6], 10, 12, 25)}\n\n`);
+}
