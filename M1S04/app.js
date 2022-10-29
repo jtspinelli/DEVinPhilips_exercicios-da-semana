4d58ee8bad3ce85e83f75bf2e4a26b85f685ccf1
jtspinelli <contato@jonathanspinelli.com>
2022-10-28 21:37:35 -0300

    add form layout func

--- M1S04/app.js
+++ M1S04/app.js
@@ -3,7 +3,13 @@ import {getPhoneUnmasked, getCpfUnmasked} from './input-masks.js'
 import {toast} from './toaster.js'
 
 let contas = [];
+const operations = document.getElementsByClassName("radio");
 const operationsLabels = document.getElementsByClassName("operations-label");
+const formServicosInputValor = document.getElementById("form-servicos-input-valor");
+
+const servicosSaque = document.getElementById("servicos-saque");
+const servicosDeposito = document.getElementById("servicos-deposito");
+const servicosSaldo = document.getElementById("servicos-saldo");
 
 formularioDeCadastro.addEventListener("submit", criaConta);
 
@@ -11,17 +17,68 @@ for(let label of operationsLabels) {
     label.addEventListener("click", selectOperation)
 }
 
+document.getElementById("voltar-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    for(let i = 0; i < operations.length; i++) {
+        if(operations[i].children[1].classList.value.includes("not-selected")) {
+            
+            document.getElementById("voltar-btn").classList.add("hidden-by-opacity")
+            document.getElementById("form-servicos-content").classList.add("fade-up")
+
+            setTimeout(() => {
+                operations[i].classList.remove("fade-up")
+                setTimeout(() => {
+                    limpaOperacaoSelecionada();
+                },350)
+            }, 400)
+
+            
+        }
+    }
+})
+
 function selectOperation(event) {
+    operationIsConsultaSaldo(event) ? escondeInputValor() : mostraInputValor();
+    limpaOperacaoSelecionada();
+    selecionaOpcaoClicada(event);  
+}
+
+function selecionaOpcaoClicada(event) {
     const respectiveRadioButton = event.target.previousElementSibling
     respectiveRadioButton.checked = true;
 
+    event.target.classList.add("selected-option")
+    event.target.classList.remove("not-selected-option")
+
+    for(let i = 0; i < operations.length; i++) {
+        if(operations[i].children[1].classList.value.includes("not-selected")) {
+            operations[i].classList.add("fade-up")
+            document.getElementById("voltar-btn").classList.remove("hidden-by-opacity")
+            setTimeout(() => {
+                document.getElementById("form-servicos-content").classList.remove("fade-up")
+            }, 300)
+            
+        }
+    }
+}
+
+function limpaOperacaoSelecionada() {
     for(let label of operationsLabels) {
         label.classList.remove("selected-option")
         label.classList.add("not-selected-option")
     }
+}
 
-    event.target.classList.toggle("selected-option")
-    event.target.classList.remove("not-selected-option")
+function operationIsConsultaSaldo(event) {
+    return event.target.innerText.toLowerCase() === 'consulta saldo';
+}
+
+function escondeInputValor() {
+    formServicosInputValor.classList.add("hidden");
+}
+
+function mostraInputValor() {
+    formServicosInputValor.classList.remove("hidden");
 }
 
 function criaConta(event) {
