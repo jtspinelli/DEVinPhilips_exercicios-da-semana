8668c8ccde1350f0bd670c21338d91c0a990960c
jtspinelli <contato@jonathanspinelli.com>
2022-10-31 18:11:23 -0300

    add ex 1 as function

--- M1S05/app.js
+++ M1S05/app.js
@@ -1,12 +1,15 @@
 import Funcionario from './Funcionario.js';
 import validaCpf from './cpf.js';
 
-const jonathan = new Funcionario('Jonathan Spinelli', '01857203070', 1000);
+window.ex1 = () => {
+    const jonathan = new Funcionario('Jonathan Spinelli', '01857203070', 1000);
 
-console.log(jonathan);
+    console.log(jonathan);
 
-jonathan.promover(50);
+    jonathan.promover(50);
 
-console.log(jonathan);
+    console.log(jonathan);
 
-console.log(`Cpf informado é válido: ${validaCpf(jonathan.cpf)}`);
+    console.log(`Cpf informado é válido: ${validaCpf(jonathan.cpf)}`);
+}
+
