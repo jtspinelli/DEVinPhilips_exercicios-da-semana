9cf7e241ed36a4cfd660f66f4c6dcf4a97bf3d48
jtspinelli <contato@jonathanspinelli.com>
2022-10-31 18:24:03 -0300

    add ex2

--- M1S05/app.js
+++ M1S05/app.js
@@ -1,4 +1,5 @@
 import Funcionario from './Funcionario.js';
+import Fatura from './Fatura.js';
 import validaCpf from './cpf.js';
 
 window.ex1 = () => {
@@ -11,5 +12,13 @@ window.ex1 = () => {
     console.log(jonathan);
 
     console.log(`Cpf informado é válido: ${validaCpf(jonathan.cpf)}`);
+}
+
+window.ex2 = () => {
+    const fatura = new Fatura(123, 'Maçã', 5, 2);
+
+    console.log(fatura);
+
+    console.log(fatura.valorTotal); 
 }
 
