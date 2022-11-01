371479c8ea813379a301f3f3d9add057a2c66562
jtspinelli <contato@jonathanspinelli.com>
2022-11-01 00:06:04 -0300

    add ex 5

--- M1S05/app.js
+++ M1S05/app.js
@@ -3,6 +3,7 @@ import validaCpf from './cpf.js';
 import Fatura from './Fatura.js';
 import Personagem from './Personagem.js';
 import {Cachorro, Gato} from './Animal.js'
+import CalculadoraDeArea from './CalculadoraDeArea.js'
 
 
 window.ex1 = () => {
@@ -45,4 +46,16 @@ window.ex4 = () => {
 
     console.log(loiro.som);
     console.log(luna.som);
+}
+
+window.ex5 = () => {
+    const triangulo = new CalculadoraDeArea('triangulo', 6, 8);
+    const areaDoTriangulo = triangulo.calcular();
+    const quadrado = new CalculadoraDeArea('quadrado', 4, 4);
+    const areaDoQuadrado = quadrado.calcular();
+    const retangulo = new CalculadoraDeArea('retangulo', 4, 6);
+    const areaDoRetangulo = retangulo.calcular();
+    console.log(areaDoTriangulo);
+    console.log(areaDoQuadrado);
+    console.log(areaDoRetangulo);
 }
