b6d83998d5565edf446cc2828e1f24a821179e9a
jtspinelli <contato@jonathanspinelli.com>
2022-11-01 14:55:10 -0300

    move ex1 and ex2 to script folder

--- M1S05/app.js
+++ M1S05/app.js
@@ -1,6 +1,3 @@
-import Funcionario from './Funcionario.js';
-import validaCpf from './cpf.js';
-import Fatura from './Fatura.js';
 import Personagem from './Personagem.js';
 import {Cachorro, Gato} from './Animal.js'
 import CalculadoraDeArea from './CalculadoraDeArea.js'
@@ -8,27 +5,6 @@ import Juros from './Juros.js';
 import Time from './Time.js';
 import Partida from './Partida.js';
 import Usuario from './Usuario.js';
-
-
-window.ex1 = () => {
-    const jonathan = new Funcionario('Jonathan Spinelli', '01857203070', 1000);
-
-    console.log(jonathan);
-
-    jonathan.promover(50);
-
-    console.log(jonathan);
-
-    console.log(`Cpf informado é válido: ${validaCpf(jonathan.cpf)}`);
-}
-
-window.ex2 = () => {
-    const fatura = new Fatura(123, 'Maçã', 5, 2);
-
-    console.log(fatura);
-
-    console.log(fatura.valorTotal); 
-}
 
 window.ex3 = () => {
     const jonathan = new Personagem("Jonathan");
@@ -105,5 +81,82 @@ const ex8 = (event) => {
     
 }
 
+// Variáveis:
+let text = "";
 const formAutenticacao = document.getElementById("form-autenticacao");
+const titulosExerciciosM1S05 = document.querySelectorAll('ul#m1s05 li .exercise-title');
+const runButtons = document.querySelectorAll('ul#m1s05 button.run-button');
+const showCodeButtons = document.querySelectorAll('ul#m1s05 button.code-button');
+const showCodeTitle = document.getElementById("code-title");
+const showCodeContent = document.getElementById("code");
+const showCodeContainer = document.getElementsByClassName("code-container")[0];
+const showCodeCloseButton = document.getElementById("code-close");
+
+
+// Event Listeners:
 formAutenticacao.addEventListener("submit", ex8);
+
+showCodeCloseButton.addEventListener("click", closeCode);
+
+for(let title of titulosExerciciosM1S05) {
+    title.addEventListener('click', showExercise);
+}
+
+for(let btn of runButtons) {
+    btn.addEventListener('click', runExercise);
+}
+
+for(let btn of showCodeButtons) {
+    btn.addEventListener('click', showCode)
+}
+
+showCodeContainer.addEventListener("click", closeIfClickedOutside);
+
+
+// Funções:
+function showExercise(event){
+    let descriptionSection = event.target.nextElementSibling;
+    descriptionSection.classList.toggle("hidden");  
+}
+
+function runExercise(event){
+    const clickedExerciseNumber = event.target.parentElement.parentElement.parentElement.id.slice(-1);
+    window[`exercicio${clickedExerciseNumber}`]();
+}
+
+function readTextFile(file) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+
+    rawFile.onreadystatechange = () => {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                text = rawFile.responseText;
+            }
+        }
+    }
+
+    rawFile.send(null);
+}
+
+function showCode(event) {
+    const clickedExerciseNumber = event.target.parentElement.parentElement.parentElement.id.slice(-1)
+
+    readTextFile(`./scripts/ex${clickedExerciseNumber}.js`);
+
+    showCodeTitle.textContent = `Código do exercício #${clickedExerciseNumber}`;
+
+    showCodeContent.innerHTML = hljs.highlight(`
+${text}`, {language: 'javascript'}).value;
+
+showCodeContainer.classList.toggle("hidden-code");
+}
+
+function closeCode () {
+    showCodeContainer.classList.toggle("hidden-code");
+}
+
+function closeIfClickedOutside(event) {
+    let clickedOutsideBox = event.target.classList.value.includes("code-container");    
+    if(clickedOutsideBox) closeCode();
+}
